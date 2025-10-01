03faa535f14298f03dd5d6a504dcdd5ec675f571
Egor Semenov <egor_semenov_00@bk.ru>
2025-10-01 16:20:50 +0200

    test/OrangeTest: add test for Orange.java

---
+++ Panier/src/test/java/fr/ufrsciencestech/panier/OrangeTest.java
@@ -0,0 +1,21 @@
+package fr.ufrsciencestech.panier;
+
+import junit.framework.TestCase;
+
+public class OrangeTest extends TestCase {
+
+    public void testSetOrigine() {
+    }
+
+    public void testTestToString() {
+    }
+
+    public void testTestEquals() {
+    }
+
+    public void testEstSansPepin() {
+    }
+
+    public void testMain() {
+    }
+}
