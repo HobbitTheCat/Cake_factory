23159a6269338fe750a087b9e9a89fc2fa3926ad
Kelian Teixeira de melo <Kelian_Teixeira-De-Melo@etu.ube.fr>
2025-10-03 11:26:29 +0200

    ajout de la methode ajoute

--- Panier/src/main/java/fr/ufrsciencestech/panier/Panier.java
+++ Panier/src/main/java/fr/ufrsciencestech/panier/Panier.java
@@ -115,7 +115,7 @@ public class Panier {
      * @throws PanierPleinException Si le panier est plein
      */
     public void ajoute(Fruit o) throws PanierPleinException{  
-       //à compléter
+       fruits.add(o);                           //ajout d'un fruit o à la liste de fruir du panier
     }
     
     /**
