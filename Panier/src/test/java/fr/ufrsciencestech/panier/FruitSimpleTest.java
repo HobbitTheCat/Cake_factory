e2625367463e5117a3b865687410adb6ddd7beab
Leo <Leo@gmail.com>
2025-10-21 16:23:01 +0200

    feature/FruitSimpleTest

--- Panier/src/test/java/fr/ufrsciencestech/panier/FruitSimpleTest.java
+++ Panier/src/test/java/fr/ufrsciencestech/panier/FruitSimpleTest.java
@@ -10,84 +10,31 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.*;
+import org.mockito.ArgumentCaptor;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
 
 /**
  *
  * @author Léo P
  */
 public class FruitSimpleTest {
-    
-    public FruitSimpleTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of getPrix method, of class FruitSimple.
-     */
-    @Test
-    public void testGetPrix() {
-        System.out.println("getPrix");
-        FruitSimple instance = new FruitSimpleImpl();
-        double expResult = 0.0;
-        double result = instance.getPrix();
-        assertEquals(expResult, result, 0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getOrigine method, of class FruitSimple.
-     */
-    @Test
-    public void testGetOrigine() {
-        System.out.println("getOrigine");
-        FruitSimple instance = new FruitSimpleImpl();
-        String expResult = "";
-        String result = instance.getOrigine();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of setPrix method, of class FruitSimple.
      */
     @Test
     public void testSetPrix() {
-        System.out.println("setPrix");
-        double prix = 0.0;
-        FruitSimple instance = new FruitSimpleImpl();
-        instance.setPrix(prix);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setOrigine method, of class FruitSimple.
-     */
-    @Test
-    public void testSetOrigine() {
-        System.out.println("setOrigine");
-        String origine = "";
-        FruitSimple instance = new FruitSimpleImpl();
-        instance.setOrigine(origine);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        double prix = -1.0;
+        FruitSimple a = mock(FruitSimple.class);
+        a.setPrix(prix);
+        assertEquals(1,a.getPrix());
     }
 
     /**
@@ -95,63 +42,30 @@ public class FruitSimpleTest {
      */
     @Test
     public void testInitAttributs() {
-        System.out.println("initAttributs");
-        double prix = 0.0;
-        String origine = "";
-        FruitSimple instance = new FruitSimpleImpl();
-        instance.initAttributs(prix, origine);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        double prix = -1.0;
+        String origine = "Espagne";
+        FruitSimple a = mock(FruitSimple.class);
+        a.initAttributs(prix,origine);
+        assertEquals(1,a.getPrix());
     }
-
     /**
      * Test of equals method, of class FruitSimple.
      */
     @Test
     public void testEquals() {
-        System.out.println("equals");
-        Object o = null;
-        FruitSimple instance = new FruitSimpleImpl();
-        boolean expResult = false;
-        boolean result = instance.equals(o);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        FruitSimple a = mock(FruitSimple.class);when(a.getOrigine()).thenReturn("Espagne");when(a.getPrix()).thenReturn(1.0);
+        FruitSimple b = mock(FruitSimple.class);when(b.getOrigine()).thenReturn("Espagne");when(b.getPrix()).thenReturn(2.0);
+        FruitSimple c = mock(FruitSimple.class);when(c.getOrigine()).thenReturn("France");when(c.getPrix()).thenReturn(1.0);
+        FruitSimple d = mock(FruitSimple.class);when(d.getOrigine()).thenReturn("Espagne");when(d.getPrix()).thenReturn(1.0);
+
+        assertFalse(a.equals(b));
+        assertFalse(a.equals(c));
+        assertTrue(a.equals(d));
+        assertTrue(a.equals(a));
     }
 
     /**
      * Test of toString method, of class FruitSimple.
      */
-    @Test
-    public void testToString() {
-        System.out.println("toString");
-        FruitSimple instance = new FruitSimpleImpl();
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of estSansPepin method, of class FruitSimple.
-     */
-    @Test
-    public void testEstSansPepin() {
-        System.out.println("estSansPepin");
-        FruitSimple instance = new FruitSimpleImpl();
-        boolean expResult = false;
-        boolean result = instance.estSansPepin();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    public class FruitSimpleImpl extends FruitSimple {
-
-        public boolean estSansPepin() {
-            return false;
-        }
-    }
     
 }
