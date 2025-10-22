9bdc3d22f348f0da16c8fdfc19bc16e7e1ee3700
Egor Semenov <egor_semenov_00@bk.ru>
2025-10-22 17:16:01 +0200

    Creation de la classe Macedoine

---
+++ Panier/src/main/java/fr/ufrsciencestech/panier/Macedoine.java
@@ -0,0 +1,42 @@
+package fr.ufrsciencestech.panier;
+
+import java.util.ArrayList;
+
+public class Macedoine implements Fruit{
+    private final ArrayList<Fruit> fruits;
+    private String origin;
+    private boolean isSeedless;
+
+    public String getOrigine() {return this.origin;}
+
+    public Macedoine(Fruit fruit) {
+        this.fruits = new ArrayList<>();
+        this.fruits.add(fruit);
+        this.isSeedless = fruit.estSansPepin();
+    }
+
+    public void addFruit(Fruit fruit) {
+        this.fruits.add(fruit);
+        if (!fruit.estSansPepin()) this.isSeedless = false;
+    }
+
+    public boolean estSansPepin() {
+        return this.isSeedless;
+    }
+
+    public double getPrix() {
+        double price = 0;
+        for (Fruit fruit : this.fruits) {
+            price+=fruit.getPrix();
+        }
+        return price;
+    }
+
+    public String toString(){
+        StringBuilder macedoine = new StringBuilder();
+        for (Fruit fruit : this.fruits) {
+            macedoine.append(fruit.toString());
+        }
+        return macedoine.toString();
+    }
+}
